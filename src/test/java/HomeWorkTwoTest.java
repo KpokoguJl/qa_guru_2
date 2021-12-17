d3a29ad5e16d0f2479be69d222d4104455b33206
Chikov Sergey <chekistne@mail.ru>
2021-12-17 10:37:44 +0300

    Fix

--- src/test/java/HomeWorkTwoTest.java
+++ src/test/java/HomeWorkTwoTest.java
@@ -19,27 +19,27 @@ public class HomeWorkTwoTest {
 
         Configuration.browserSize = "1920x1080";
         open("https://demoqa.com/automation-practice-form");
-        $x("//input[@id='firstName']").setValue(firstName);
-        $x("//input[@id='lastName']").setValue(lastName);
-        $x("//input[@id='userEmail']").setValue(email);
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(email);
         $x("//label[contains(text(),'Male')]").click();
-        $x("//input[@id='userNumber']").setValue(phone);
-        $x("//input[@id='dateOfBirthInput']").click();
+        $("#userNumber").setValue(phone);
+        $("#dateOfBirthInput").click();
         $(".react-datepicker__year-select").selectOption("2000");
         $(".react-datepicker__month-select").selectOption("January");
         $("[aria-label='Choose Saturday, January 1st, 2000']").click();
         $("#subjectsInput").setValue("English").pressEnter();
         $x("//label[contains(text(),'Sports')]").click();
         File file = new File("src/test/testdata/file.txt");
-        $x("//input[@id='uploadPicture']").uploadFile(file);
-        $x("//textarea[@id='currentAddress']").setValue(address);
+        $("#uploadPicture").uploadFile(file);
+        $("#currentAddress").setValue(address);
         $x("//div[contains(text(),'Select State')]").click();
         $x("//div[contains(text(),'NCR')]").click();
         $x("//div[contains(text(),'Select City')]").click();
         $x("//div[contains(text(),'Delhi')]").click();
-        $x("//button[@id='submit']").click();
+        $("#submit").click();
 
-        $x("//div[@id='example-modal-sizes-title-lg']").shouldBe(visible);
+        $("#example-modal-sizes-title-lg").shouldBe(visible);
         $x("//tbody/tr[1]/td[2]").shouldHave(text(firstName + ' ' + lastName));
         $x("//tbody/tr[2]/td[2]").shouldHave(text(email));
         $x("//tbody/tr[3]/td[2]").shouldHave(text("Male"));
